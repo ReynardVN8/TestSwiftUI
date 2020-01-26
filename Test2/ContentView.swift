31a458c2f69a8f43dbe68956ceaa0cb30da3b1eb
ReynardVN8 <reynardvnata@yahoo.co.id>
2020-01-26 20:40:13 +0700

    Initial Commit

---
+++ Test2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test2
+//
+//  Created by Reynard Vincent Nata on 26/01/20.
+//  Copyright © 2020 ReynardVincent. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
